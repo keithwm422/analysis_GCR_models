efe7a366f69cecc9bcbd4963f3559c3c15a56039
keithwm422 <keithwm.422@gmail.com>
2021-01-25 10:50:14 -0500

    Working chisquare calculation with low rigidity cutoff imposed for B/C

--- SC_python_code/B_C_chisquare_cutoff.py
+++ SC_python_code/B_C_chisquare_cutoff.py
@@ -28,6 +28,7 @@ from open_tarfile import get_fluxes_from_files
 # Example function for testing this procedure before expanding to many ratios
 def run_chi_square_test(seq,num_spline_steps,cutoff):
     # FIRST THE DATA
+    MAX_DIFFUSION=20
     numerator='B'  # numerator of the nuclei ratio used in the chi_square calculation (needs to also be AMS_DATA file first character)
     denominator='C'  # same as numerator but denominator for the nuclei ratio
     df=read_in_data(numerator,denominator) #read in the AMS data for the given ratio as a pandas dataframe
@@ -49,8 +50,12 @@ def run_chi_square_test(seq,num_spline_steps,cutoff):
     #print(f'ENERGY ARRAY: {energy}')
     # BIG LOOP TO GET ALL 400 models in sets of 20.
     chi_square_array=[]
+    chi_square_nparray=np.empty([20,20])
+    model_name_full=np.empty([20,20],dtype="S10")
+    stats_full=np.empty([20,20,2]) # the stats object is a scipy chiqsuare object which has two components a chisquare and a pvalue
+    stats_masked_full=np.empty([20,20,2]) # the stats object is a scipy chiqsuare object which has two components a chisquare and a pvalue
     diffusion_number=1 #(set to [1,20])
-    while diffusion_number<21:
+    while diffusion_number<MAX_DIFFUSION+1:
         chi_square_temp=[]  # reset this one at each iteration
         #first arg is not used currently for the following function (so it can be anything really)
         fluxes_per_element_per_diffusion=get_fluxes_from_files(1, diffusion_number)  # to store the loaded fluxes from GALPROP sims
@@ -59,8 +64,11 @@ def run_chi_square_test(seq,num_spline_steps,cutoff):
         # the energy array is the same for all isotopes
         #need to pass the log fluxes found from the models above like so:
         halo_model=0 # halo size is weird, since it reads them in alphebatical order. model=0 is L=10, model=1 L=11, .. model=10 L=1, model=11 L=20, model=12 L=2 ... model=19 L=9 
-        ratios_splined_per_diffusion=[]
-        chi_square_array_per_diffusion=[]
+        #ratios_splined_per_diffusion=np.empty([20,])
+        model_name=np.empty([20,],dtype="S10")
+        chi_square_array_per_diffusion=np.empty([20,])
+        stats_per_diffusion=np.empty([20,2]) # the stats object is a scipy chiqsuare object which has two components a chisquare and a pvalue
+        stats_masked_per_diffusion=np.empty([20,2]) # the stats object is a scipy chiqsuare object which has two components a chisquare and a pvalue
         while halo_model<20:
             # access the actual flux from the loaded simulation sets and then log them
             logB10_flux=log_energy(fluxes_per_element_per_diffusion[halo_model][cosmic_ray_nuclei_index.element_index.index(cosmic_ray_nuclei_index.boron10_loc)])
@@ -69,22 +77,44 @@ def run_chi_square_test(seq,num_spline_steps,cutoff):
             logC13_flux=log_energy(fluxes_per_element_per_diffusion[halo_model][cosmic_ray_nuclei_index.element_index.index(cosmic_ray_nuclei_index.carbon13_loc)])
             # now call function that will spline all those fluxes and the energy axis to a common rigidity range among all the isotopes. 
             rigC13_spline,B_C_ratio_spline=B_C_ratio(energy,logB10_flux,logB11_flux,logC12_flux,logC13_flux,num_spline_steps)
-            ratios_splined_per_diffusion.append(B_C_ratio_spline)
             # NOW CAN CALCULATE THE CHI-SQUARE and get residuals
             # if there is a cutoff in rigidity (use data at this value and above), then use the following
             residuals,chi_square,stats_obj,stats_obj_masked,rigidity_masked,ratio_masked,model_x,model_y,model_x_masked,model_y_masked=calculate_chi_square(rigidity,ratio,rigC13_spline, B_C_ratio_spline,cutoff)
             # these quantities need to be saved into huge arrays, but also apply the formatting inline here
-
+            # formatting is that the loaded files are done in alphabetical order, so the numbers are not in the correct order. 
+            # the print statement below shows that the first model read in is L=10, L=11, ... L=19 then L=1, L=20, then L=2, L=3....L=9
             print(f'chi-square {chi_square}, from model {fluxes_per_element_per_diffusion[halo_model][-1]}')
-
-            chi_square_array_per_diffusion.append(chi_square)
-
-            chi_square_array_per_diffusion.append(fluxes_per_element_per_diffusion[halo_model][-1])
-
+            if halo_model<=9: #this is L=10 so set it 9th position in array, ==1 L=11 set it to 10 pos in array... j==9 L=19 set it to 18 pos in array
+                chi_square_array_per_diffusion[halo_model+9]=chi_square
+                model_name[halo_model+9]=fluxes_per_element_per_diffusion[halo_model][-1]
+                #ratios_splined_per_diffusion[halo_model+9]=B_C_ratio_spline
+                stats_per_diffusion[halo_model+9]=stats_obj
+                stats_masked_per_diffusion[halo_model+9]=stats_obj_masked
+            elif halo_model==10: # this is L=1 so set it to zeroth pos
+                chi_square_array_per_diffusion[0]=chi_square
+                model_name[0]=fluxes_per_element_per_diffusion[halo_model][-1]
+                #ratios_splined_per_diffusion[0]=B_C_ratio_spline
+                stats_per_diffusion[0]=stats_obj
+                stats_masked_per_diffusion[0]=stats_obj_masked
+            elif halo_model==11: #this is L=20 so set it to last pos
+                chi_square_array_per_diffusion[19]=chi_square
+                model_name[19]=fluxes_per_element_per_diffusion[halo_model][-1]
+                #ratios_splined_per_diffusion[19]=B_C_ratio_spline
+                stats_per_diffusion[19]=stats_obj
+                stats_masked_per_diffusion[19]=stats_obj_masked
+            elif halo_model>=12:  # these are j>=12 where j==12 is L=2 so set to 1 pos in array, j==13 set it 2 in array
+                chi_square_array_per_diffusion[halo_model-11]=chi_square
+                model_name[halo_model-11]=fluxes_per_element_per_diffusion[halo_model][-1]
+                #ratios_splined_per_diffusion[halo_model-11]=B_C_ratio_spline
+                stats_per_diffusion[halo_model-11]=stats_obj
+                stats_masked_per_diffusion[halo_model-11]=stats_obj_masked
+            # other variables can be added as needed (for plotting or further investigation)
             halo_model+=1
-
+        chi_square_nparray[diffusion_number-1]=chi_square_array_per_diffusion
         chi_square_array.append(chi_square_array_per_diffusion)
-
+        model_name_full[diffusion_number-1]=model_name
+        stats_full[diffusion_number-1]=stats_per_diffusion
+        stats_masked_full[diffusion_number-1]=stats_masked_per_diffusion
         diffusion_number+=1
     #something else
     #with open("chisquarefile.txt", 'w') as file:
@@ -94,67 +124,14 @@ def run_chi_square_test(seq,num_spline_steps,cutoff):
 
     #print('{chi_square_array[0][0]})
     print("Saved file")
-    return chi_square_array
-
-def format_chi_square(seq, num_spline_steps,cutoff):
-    chi_square_array=run_chi_square_test(1,num_spline_steps,cutoff)
-    print(type(chi_square_array))
-    print(f'chi square array num rows {len(chi_square_array)}')
-    print(f'chi square array num columns {len(chi_square_array[0])}')
-    #print(chi_square_array[0]) # this is still a list since its a list of lists
-    diffusion_iter=0
-    L=[]
-    D=[]
-    chi_squares_full=[]
-    while diffusion_iter<len(chi_square_array):
-        iter=0
-        chi_squares=[]
-        while iter<len(chi_square_array[diffusion_iter]): #axis for constant diffusion coeff, so halo size axis
-            # the first value is L=10, D=1, the second value is L=11, D=1. So the first 20*2 will be D=1 and L in a strange order.
-            if (iter % 2) == 0:
-                chi_squares.append(float(chi_square_array[diffusion_iter][iter]))
-            else:
-                L_D_values=chi_square_array[diffusion_iter][iter].split("_")
-                #print(L_D_values)
-                L.append(int(L_D_values[-3]))
-                D.append(int(L_D_values[-1]))
-            iter+=1
-        # don't append the values since they are out of order. Instead, readjust them into the right order
-        j=0
-        temp=chi_squares.copy()
-        #print(f'j={j} and temp before adjustments {temp}')
-        while j<len(chi_squares):
-            if j<=9: #this is L=10 so set it 9th position in array, j==1 L=11 set it to 10 pos in array... j==9 L=19 set it to 18 pos in array
-                temp[j+9]=chi_squares[j]
-                #print(f'j={j} and j should be leq 9')
-            elif j==10: # this is L=1 so set it to zeroth pos
-                temp[0]=chi_squares[j]
-                #print(f'j={j} and j should be 10')
-            elif j==11: #this is L=20 so set it to last pos
-                temp[19]=chi_squares[j]
-                #print(f'j={j} and j should be 11')
-            elif j>=12:  # these are j>=12 where j==12 is L=2 so set to 1 pos in array, j==13 set it 2 in array
-                temp[j-11]=chi_squares[j]
-                #print(f'j={j} and j should be geq 12')
-                #print(f'j-11={j-11} and temp should be {chi_squares[j]} but temp is {temp[j-11]}')
-            j+=1
-        #print(f'j={j} and temp before adjustments {temp}')
-        chi_squares_full.append(temp)
-        diffusion_iter+=1
-    # now for each column in chi_square_full, need to readjust the halo models so they match up accurately
-    #print(chi_squares_full[0][5]) # this is still a list since its a list of lists
-    #print(L[5])
-    #print(D[5])
-    #print(chi_square_array[0][10])
-    #print(chi_square_array[0][11])
-    return chi_squares_full
+    return chi_square_array,chi_square_nparray,model_name_full,stats_full,stats_masked_full
 
 def make_colormap(seq):
     # quantities for the analysis
     num_spline_steps=2000
     kinetic_energy_cutoff=20 # GeV/nucleon
     cutoff=rigidity_calc(kinetic_energy_cutoff,13,6) # the other two args are, in order, mass (num nucleons) and charge (Z).
-    chi_squares_full=format_chi_square(1,num_spline_steps,cutoff)
+    chi_squares_full, chi_squares_full_np,models,stats_full, stats_masked_full=run_chi_square_test(1,num_spline_steps,cutoff)
     print(f'chi square full num rows {len(chi_squares_full)}')
     print(f'chi square full num columns {len(chi_squares_full[0])}')
     L_values=np.arange(1,20,20)
@@ -184,6 +161,34 @@ def make_colormap(seq):
     print("about to save")
     #plt.savefig("heatmap_example_B_C_new_1.png",dpi=400)
     plt.savefig(filepaths.images_path+'heatmap_example_B_C'+str(kinetic_energy_cutoff)+'_.png',dpi=400)
+    make_colormap_mask(stats_masked_full,kinetic_energy_cutoff)
 
+def make_colormap_mask(stats_obj_masked,kinetic_energy_cutoff):
+    font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 16}
+    matplotlib.rc('font', **font)
+    # We can visualize with a heatmap
+    sns.set_style("white")
+    fig, ax = plt.subplots(figsize=(10,10))
+    #cax=ax.matshow(sum_squares_full, cmap = 'Greens')
+    cax=ax.matshow(stats_obj_masked[:,:,0], cmap='plasma', norm=LogNorm(vmin=0.1, vmax=1000))
+    ax.tick_params(axis="x", bottom=True, top=False, labelbottom=True, labeltop=False)
+    ax.set_xticks([i for i in range(20)])
+    ax.set_xticklabels([str(i) for i in range(1,21)], fontsize=14)
+    #ax.set_yticklabels([Categories[i] for i in range(20)],fontsize=14)
+    #ax.colorbar()
+    fig.colorbar(cax)
+    ax.set_yticks([i for i in range(20)])
+    ax.set_yticklabels([str(i) for i in range(1,21)], fontsize=14)
+    #ax.set_xticklabels([str(i) for i in Categories], rotation = 90,fontsize=14)
+    ax.set_ylabel("Diffusion Coefficient "r'$10^{28}cm^{2}s^{-1}$', fontsize = 14)
+    ax.set_xlabel("Halo Size (kpc)", fontsize = 14)
+    #plt.tight_layout()
+    plt.title("Chi Square Above 20 GeV/n for B/C", y=1.08)
+    print("about to save")
+    #plt.savefig("heatmap_example_B_C_new_1.png",dpi=400)
+    plt.savefig(filepaths.images_path+'heatmap_example_B_C'+str(kinetic_energy_cutoff)+'_masked_.png',dpi=400)
+   
 
 ## NEW CODE FOR ADJUSTING THE VALUES OF CHISQUARE
